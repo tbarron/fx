f5b629df6658de6745c4a096af3954ac6cfd7d57
Tom Barron <tusculum@gmail.com>
2018-01-17 21:38:42 -0500

    Add code and tests for cmd

--- src/cmd.rs
+++ src/cmd.rs
@@ -1,4 +1,38 @@
+use std::process::Command;
+
+// ----------------------------------------------------------------------------
 pub fn cmd(command: &str, items: &[&str]) {
-    println!("{}", command);
-    println!("{:?}", items);
+    for filled in _cmdlist(command, items) {
+        let mut cvec: Vec<String> = Vec::new();
+        for item in filled.split(" ") {
+            cvec.push(String::from(item));
+        }
+        let _output = Command::new(&cvec[0])
+            .args(&cvec[1..])
+            .spawn()
+            .expect(format!("failure running '{}'", filled).as_str());
+    }
+}
+
+// ----------------------------------------------------------------------------
+fn _cmdlist(command: &str, items: &[&str]) -> Vec<String> {
+    let mut rvec: Vec<String> = Vec::new();
+    for item in items {
+        let full = String::from(command.replace('%', item));
+        rvec.push(full);
+    }
+    rvec
+}
+
+// ----------------------------------------------------------------------------
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // ------------------------------------------------------------------------
+    #[test]
+    fn test_cmd_make_list() {
+        assert_eq!(_cmdlist("echo %", &["foo", "bar", "xyzzy"]),
+                   ["echo foo", "echo bar", "echo xyzzy"]);
+    }
 }

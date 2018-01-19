e427633152dff3a4f1a06cbbcf3492054f3ce5da
Tom Barron <tusculum@gmail.com>
2018-01-19 11:08:54 -0500

    Single source of truth for the names of unprintable ASCII characters

--- src/ascii.rs
+++ src/ascii.rs
@@ -1,40 +1,6 @@
-// NAMES:
-//     ["NUL", "SOH", "STX", "ETX", "EOT", "ENQ", "ACK", "BEL", "BS",
-//     "TAB", "LF", "VT", "FF", "CR", "SO", "SI", "DLE", "DC1", "DC2",
-//     "DC3", "DC4", "NAK", "SYN", "ETB", "CAN", "EM", "SUB", "ESC",
-//     "FS", "GS", "RS", "US", "SPC"];
-
-
-
 // ----------------------------------------------------------------------------
 // Display the ascii code table
 pub fn ascii() {
-    // let mut line = String::from("");
-    // let names = ["NUL", "SOH", "STX", "ETX", "EOT", "ENQ", "ACK", "BEL",
-    //              "BS",  "TAB", "LF",  "VT",  "FF",  "CR",  "SO",  "SI",
-    //              "DLE", "DC1", "DC2", "DC3", "DC4", "NAK", "SYN", "ETB",
-    //              "CAN", "EM",  "SUB", "ESC", "FS",  "GS",  "RS",  "US",
-    //              "SPC"];
-    // let mut ldx = 0;
-    // let mut ord: usize;
-    // while ldx < 0x80 {
-    //     for off in 0..8 {
-    //         ord = ldx + off;
-    //         if ord <= 0x20 {
-    //             let prt = names[ord];
-    //             line = format!("{0}0x{1:02x} {chr:<wid$} ",
-    //                            line, ord, chr=prt, wid=3);
-    //         } else if ord < 0x7f {
-    //             let charval: u8 = ord as u8;
-    //             let prt = charval as char;
-    //             line = format!("{0}0x{1:02x} {chr:<wid$} ",
-    //                            line, ord, chr=prt, wid=3);
-    //         }
-    //     }
-    //     println!("{}", line);
-    //     line = String::from("");
-    //     ldx += 8;
-    // }
     println!("{}", ascii_text());
 }
 
@@ -44,18 +10,14 @@ fn ascii_text() -> String {
     let mut line = String::from("");
     let mut rval = String::from("");
     let mut sep = String::from("");
-    let names = ["NUL", "SOH", "STX", "ETX", "EOT", "ENQ", "ACK", "BEL",
-                 "BS",  "TAB", "LF",  "VT",  "FF",  "CR",  "SO",  "SI",
-                 "DLE", "DC1", "DC2", "DC3", "DC4", "NAK", "SYN", "ETB",
-                 "CAN", "EM",  "SUB", "ESC", "FS",  "GS",  "RS",  "US",
-                 "SPC"];
+    let names = ascii_names();
     let mut ldx = 0;
     let mut ord: usize;
     while ldx < 0x80 {
         for off in 0..8 {
             ord = ldx + off;
             if ord <= 0x20 {
-                let prt = names[ord];
+                let prt = &names[ord];
                 line = format!("{0}0x{1:02x} {chr:<wid$} ",
                                line, ord, chr=prt, wid=3);
             } else if ord < 0x7f {
@@ -74,13 +36,17 @@ fn ascii_text() -> String {
 }
 
 // ----------------------------------------------------------------------------
-// fn ascii_names() -> [str; 4] {
-//     ["NUL", "SOH", "STX", "ETX", "EOT", "ENQ", "ACK", "BEL",
-//      "BS",  "TAB", "LF",  "VT",  "FF",  "CR",  "SO",  "SI",
-//      "DLE", "DC1", "DC2", "DC3", "DC4", "NAK", "SYN", "ETB",
-//      "CAN", "EM",  "SUB", "ESC", "FS",  "GS",  "RS",  "US",
-//      "SPC"]
-// }
+fn ascii_names() -> Vec<String> {
+    let mut rval = Vec::new();
+    for item in ["NUL", "SOH", "STX", "ETX", "EOT", "ENQ", "ACK", "BEL",
+                 "BS",  "TAB", "LF",  "VT",  "FF",  "CR",  "SO",  "SI",
+                 "DLE", "DC1", "DC2", "DC3", "DC4", "NAK", "SYN", "ETB",
+                 "CAN", "EM",  "SUB", "ESC", "FS",  "GS",  "RS",  "US",
+                 "SPC"].iter() {
+        rval.push(String::from(*item));
+    }
+    rval
+}
 
 // ----------------------------------------------------------------------------
 #[cfg(test)]
@@ -110,27 +76,24 @@ mod tests {
     // ascii_names() that returns the array used below (and also in
     // ascii_text(). But I haven't figured out how to get rust to let
     // me do that yet. :(
+    //
+    // Update: The trick was to write the function to return a Vec of String.
     #[test]
     fn test_ascii_low() {
-        let names = ["NUL", "SOH", "STX", "ETX", "EOT", "ENQ", "ACK", "BEL",
-                     "BS",  "TAB", "LF",  "VT",  "FF",  "CR",  "SO",  "SI",
-                     "DLE", "DC1", "DC2", "DC3", "DC4", "NAK", "SYN", "ETB",
-                     "CAN", "EM",  "SUB", "ESC", "FS",  "GS",  "RS",  "US",
-                     "SPC"];
         let text = ascii_text();
-
+        let names = ascii_names();
         for name in names.iter() {
             assert!(text.contains(name));
         }
     }
 
     // ------------------------------------------------------------------------
-    // #[test]
-    // fn test_ascii_names() {
-    //     let text = ascii_text();
-    //     for item in ascii_names() {
-    //         let spitem = String::from(" " + item);
-    //         assert!(text.contains(spitem))
-    //     }
-    // }
+    #[test]
+    fn test_ascii_names() {
+        let text = ascii_text();
+        for item in 0x21..0x7f {
+            let mut spitem = format!("0x{:x}", item);
+            assert!(text.contains(spitem.as_str()))
+        }
+    }
 }

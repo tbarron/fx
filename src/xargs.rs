887561f316468817e90b4f4d1a130e3c58927c22
Tom Barron <tusculum@gmail.com>
2018-01-25 07:15:15 -0500

    Update the xargs stub to accept and report all its arguments

--- src/xargs.rs
+++ src/xargs.rs
@@ -1,7 +1,12 @@
 // ----------------------------------------------------------------------------
-// Stub for subcommand xargs
+// This function reads tokens from stdin and inserts them into
+// *command* where the '%' is, limiting the total length of command
+// lines to around 250 bytes. With *dryrun*, just report the generated
+// commands without actually running them. With *verbose*, report the
+// generated commands before running each one.
 //
-pub fn xargs(dryrun: bool, command: &str) {
+pub fn xargs(dryrun: bool, verbose: bool, command: &str) {
     println!("dryrun = {:?}", dryrun);
+    println!("verbose = {:?}", verbose);
     println!("command = '{}'", command);
 }

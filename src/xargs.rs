fc353a4d78e03761be90c682112b2ca21e19a226
Tom Barron <tusculum@gmail.com>
2018-01-25 09:32:57 -0500

    Code to satisfy test_xargs_dryrun

--- src/xargs.rs
+++ src/xargs.rs
@@ -2,16 +2,44 @@ use super::*;
 
 // ----------------------------------------------------------------------------
 // This function reads tokens from stdin and inserts them into
-// *command* where the '%' is, limiting the total length of command
-// lines to around 250 bytes. With *dryrun*, just report the generated
-// commands without actually running them. With *verbose*, report the
-// generated commands before running each one.
+// *command* where the replstr ('%' by default) is, limiting the total
+// length of command lines to 256 bytes or less. With *dryrun*, just
+// report the generated commands without actually running them. With
+// *verbose*, report the generated commands before running each one.
 //
 pub fn xargs(dryrun: bool, verbose: bool, command: &str) {
-    println!("dryrun = {:?}", dryrun);
-    println!("verbose = {:?}", verbose);
-    println!("command = '{}'", command);
+    let cl: Vec<String> = _cmdlist(command);
+    for cmd in cl {
+        if dryrun {
+            println!("Would run '{}'", cmd);
+        }
+    }
+}
 
+// ----------------------------------------------------------------------------
+// Constructs and returns a list of commands from *command* and stdin,
+// putting strings of tokens into *command* in place of the replstr
+// ('%' by default)
+//
+fn _cmdlist(command: &str) -> Vec<String> {
     let stdin = read_file("<stdin>");
-    println!("{}", stdin);
+    let mut rval: Vec<String> = Vec::new();
+    let mut collect = String::new();
+    for item in stdin.split_whitespace() {
+        if item.len() + 1 + collect.len() + command.len() < 256 {
+            if 0 < collect.len() { collect.push_str(" "); }
+            collect.push_str(item);
+        } else {
+            let cmd: String = command.replace('%', collect.as_str());
+            rval.push(cmd);
+            collect = String::from(item);
+        }
+    }
+
+    if 0 < collect.len() {
+        let cmd: String = command.replace('%', collect.as_str());
+        rval.push(cmd);
+    }
+
+    rval
 }

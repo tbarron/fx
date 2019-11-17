087653d70a08a8a0ccc185cd3953574256c0d11a
Tom Barron <tusculum@gmail.com>
2019-11-17 17:16:21 -0500

    0.0.4 rc5: Tweaks to make fx install work

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-from setuptools import setup
+from fx import version
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-def execfile(fname, gdict, ldict):
-    exec(compile(open(fname, 'rb').read(), fname, 'exec'),
-         gdict,
-         ldict)
-execfile("./fx/version.py", globals(), locals())
-
-setup(name="fx",
-      version=__version__,
-      author="Tom Barron",
-      author_email="tusculum@gmail.com",
-      entry_points={'console_scripts': ["fx = fx:main"]}
-      )
+setup(
+    name="fx",
+    description='Command line special effects',
+    version=version.__version__,
+    author="Tom Barron",
+    author_email="tusculum@gmail.com",
+    packages=['fx'],
+    scripts=[],
+)

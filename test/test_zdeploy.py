5717fb9c84bad3cce477ae6e6caece130e806054
Tom Barron <tusculum@gmail.com>
2019-11-18 15:26:22 -0500

    Get rid of GitPython and use tbx instead

--- test/test_zdeploy.py
+++ test/test_zdeploy.py
@@ -1,10 +1,6 @@
 from fx import version
 import pytest
-skip_deployable = False
-try:
-    from git import Repo
-except ImportError:
-    skip_deployable = True
+import tbx
 
 
 # -----------------------------------------------------------------------------
@@ -13,17 +9,17 @@ def test_deployable():
     Check current version against last git tag and check for outstanding
     untracked files
     """
-    if skip_deployable:
-        pytest.skip()
-    r = Repo('.')
-    assert [] == r.untracked_files, "You have untracked files"
-    assert [] == r.index.diff(r.head.commit), "You have staged updates"
-    assert [] == r.index.diff(None), "You have uncommited changes"
+    pytest.dbgfunc()
+    staged, changed, untracked = tbx.git_status()
+    assert untracked == [], "You have untracked files"
+    assert changed == [], "You have unstaged updates"
+    assert staged == [], "You have updates staged but not committed"
 
-    stags = sorted(r.tags,
-                   key=lambda x: x.commit.committed_date,
-                   reverse=True)
+    if tbx.git_current_branch() != 'master':
+        return True
 
-    assert version.__version__ == str(stags[0]), "Version does not match tag"
-
-    assert str(r.head.commit) == str(stags[0].commit), "Tag != HEAD"
+    last_tag = tbx.git_last_tag()
+    msg = "Version ({}) does not match tag ({})".format(tbx.version(),
+                                                        last_tag)
+    assert version() == last_tag, msg
+    assert tbx.git_hash() == tbx.git_hash(last_tag), "Tag != HEAD"
